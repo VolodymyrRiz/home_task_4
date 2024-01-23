1fbc84135ec746b2847846b76c2ced946ca901a6
VolodymyrRiz <v.v.rizun@gmail.com>
2024-01-23 23:26:18 +0200

    ht4

--- front-init/main.py
+++ front-init/main.py
@@ -1,1 +1,25 @@
 # ДЗ 4
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class WebDodatok_HT4(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'hhhhhh')
+        
+            
+    def do_POST(self):
+        pass
+    
+def run_server():
+    address = ('localhost', 8080)
+    http_server = HTTPServer(address, WebDodatok_HT4)
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
+        
+if __name__ == '__main__':
+    run_server()

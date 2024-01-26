1b681b745d36737914cc620f85e5f99064b0f29d
VolodymyrRiz <v.v.rizun@gmail.com>
2024-01-27 01:56:06 +0200

    ht6

--- front-init/main.py
+++ front-init/main.py
@@ -1,7 +1,11 @@
 # ДЗ 4
+import mimetypes
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
 import pathlib
+import json
+import datetime
+
 
 class WebDodatok_HT4(BaseHTTPRequestHandler):
     
@@ -27,7 +31,7 @@ class WebDodatok_HT4(BaseHTTPRequestHandler):
         self.end_headers()
         with open("c:/Users/IJHEA/Documents/Python/home_task_4/front-init/message.html", 'rb') as file:
             self.wfile.write(file.read())
-            print('jkkjkjkjkjkjkkk')
+            
             
     def send_html_file_2(self, file, status=200):
         self.send_response(status)
@@ -35,10 +39,29 @@ class WebDodatok_HT4(BaseHTTPRequestHandler):
         self.end_headers()
         with open("c:/Users/IJHEA/Documents/Python/home_task_4/front-init/error.html", 'rb') as file:
             self.wfile.write(file.read())
-            print('2222222222222222222222')
+            
             
     def do_POST(self):
-        pass
+        data_dict_1 = {}           
+            
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+        with open('c:/Users/IJHEA/Documents/Python/home_task_4/front-init/storage/data.json', 'r') as fil:
+            data_dict_1 = json.load(fil)
+            
+        print(data_dict_1)    
+            
+        data_dict_1.update({str(datetime.datetime.now()): data_dict})
+        with open('c:/Users/IJHEA/Documents/Python/home_task_4/front-init/storage/data.json', 'w', encoding='utf-8') as fil:
+            json.dump(data_dict_1, fil)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
     
 def run_server():
     address = ('localhost', 3000)
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-print("rerere")
-print("dkkk;l;l;kdd")
+import mimetypes
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+import pathlib
+import json
+import datetime
+data_dict_1 = None
+
+d = {}
+
+#with open('c:/Users/IJHEA/Documents/Python/home_task_4/front-init/storage/data.json', 'r') as fil:
+s = open('c:/Users/IJHEA/Documents/Python/home_task_4/front-init/storage/data.json')
+d(s)    
+s.close()
+print(d)
+
+
